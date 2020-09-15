f6dc39c7c332488a8d9b491c496d7340ef579ddd
unais ali <51183141+unaisshazan@users.noreply.github.com>
2020-09-16 04:33:01 +0500

    Update Message.js

--- src/Message.js
+++ src/Message.js
@@ -2,9 +2,21 @@ import React from 'react'
 
 function Message(props) {
     return (
-        <div>
+        <div> 
             <h2>{props.username}:{props.text}</h2>
+        <Card>
+            <CardContent>
+                <Typography
+                color="white"
+                variant="h5"
+                component="h2"
+                >
+                    {props.text}
+                </Typography>
+            </CardContent>
+        </Card>
         </div>
+        
     )
 }
 
