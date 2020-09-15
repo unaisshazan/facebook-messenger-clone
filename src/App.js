6e6f96fdba4745e1d6a02ed7f2d38cbd47ee377a
unais ali <51183141+unaisshazan@users.noreply.github.com>
2020-09-16 03:51:09 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,36 +1,49 @@
-import React,{useState} from 'react';
-import './App.css';
-import Button from '@material-ui/core/Button';
-
+import React, { useState } from "react";
+import "./App.css";
+import Button from "@material-ui/core/Button";
+import FormControl from "@material-ui/core/FormControl";
 function App() {
-  const [input,setInput]= useState(["hello"]);
-  const [messages,setMessages]=useState([]);
-  console.log(input)
-  console.log(messages)
-  const sendMessage =(event)=>{
+  const [input, setInput] = useState(["hello"]);
+  const [messages, setMessages] = useState([]);
+  console.log(input);
+  console.log(messages);
+  const sendMessage = (event) => {
     event.preventDefault();
 
-setMessages([...messages , input])
-setInput('');
-
-  }
+    setMessages([...messages, input]);
+    setInput("");
+  };
   return (
     <div className="App">
-    
-   <h1>Messenger</h1>
-   <form action="">
-   <input value={input} onChange={event =>setInput(event.target.value)}/>
-  
-   <Button type='submit' variant="contained" color="primary"  disabled={!input} onClick={sendMessage}>Send Message</Button>
-   </form>
-  
-   {
-     messages.map(message =>(
-       <p>{message}</p>
-     ))
-   }
-   
-   </div>
+      <h1>Messenger</h1>
+      <form action="">
+        <FormControl>
+          <InputLabel htmlFor="my-input">Email address</InputLabel>
+          <Input id="my-input" aria-describedby="my-helper-text" />
+          <FormHelperText id="my-helper-text">
+            We'll never share your email.
+          </FormHelperText>
+        </FormControl>
+        <input
+          value={input}
+          onChange={(event) => setInput(event.target.value)}
+        />
+
+        <Button
+          type="submit"
+          variant="contained"
+          color="primary"
+          disabled={!input}
+          onClick={sendMessage}
+        >
+          Send Message
+        </Button>
+      </form>
+
+      {messages.map((message) => (
+        <p>{message}</p>
+      ))}
+    </div>
   );
 }
 
