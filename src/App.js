542e775907600ac941bac707b4928b494c331a9a
unais ali <51183141+unaisshazan@users.noreply.github.com>
2020-09-16 04:25:19 +0500

    add

--- src/App.js
+++ src/App.js
@@ -4,12 +4,12 @@ import Button from "@material-ui/core/Button";
 import { FormControl, InputLabel, Input } from "@material-ui/core";
 import Message from "./Message";
 function App() {
-  const [input, setInput] = useState([
+  const [input, setInput] = useState([]);
+  const [messages, setMessages] = useState([
     { username: "sonny", text: "hi" },
     { username: "ali", text: "hell0" },
     { username: "areej", text: "toni" },
   ]);
-  const [messages, setMessages] = useState([]);
   const [username, setUsername] = useState("");
 
   useEffect(() => {
@@ -19,7 +19,7 @@ function App() {
   const sendMessage = (event) => {
     event.preventDefault();
 
-    setMessages([...messages, input]);
+    setMessages([...messages, {username:username,text: input}]);
     setInput("");
   };
   return (
