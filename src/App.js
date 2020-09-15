0f6eb08b563f66a24881d7f3f1bffab45bb3e1f2
unais ali <51183141+unaisshazan@users.noreply.github.com>
2020-09-15 23:13:55 +0500

    add

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React,{useState} from 'react';
 import './App.css';
 
 function App() {
+  const [input,setInput]= useState('');
   return (
     <div className="App">
     
