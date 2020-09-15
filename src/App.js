ad546fbe89efd986f2f067d594098c31c41dde56
unais ali <51183141+unaisshazan@users.noreply.github.com>
2020-09-16 03:27:20 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React,{useState} from 'react';
 import './App.css';
 
 function App() {
-  const [input,setInput]= useState('');
+  const [input,setInput]= useState(["hello"]);
   const [messages,setMessages]=useState([]);
   console.log(input)
   console.log(messages)
@@ -19,6 +19,11 @@ setInput('');
    <input value={input} onChange={event =>setInput(event.target.value)}/>
    
    <button onClick={sendMessage}>Send Message</button> 
+   {
+     messages.map(message =>(
+       <p>{message}</p>
+     ))
+   }
    
    </div>
   );
